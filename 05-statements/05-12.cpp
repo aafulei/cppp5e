9ad256a7d614077df882183f3d00ef36febdb684
Aaron Fu Lei <aaron.fu@alumni.ust.hk>
2022-01-29 12:30:21 +0800

    Update 05-12.cpp

--- 05-statements/05-12.cpp
+++ 05-statements/05-12.cpp
@@ -1,62 +1,71 @@
+// 22/01/29 = Sat
 // 18/02/04 = Sun
 
-// Exercise 5.12: Modify our vowel-counting program so that it counts the number of occurrences of the following two-character sequences: ff, fl, and fi.
+// Exercise 5.12: Modify our vowel-counting program so that it counts the number
+// of occurrences of the following two-character sequences: ff, fl, and fi.
 
+// Modified from 05-11.cpp
 
-#include <cstring>
 #include <iostream>
-#include <string>
-
-using std::cin;
-using std::cout;
-using std::getline;
-using std::string;
-using std::tolower;
-using std::std::endl;
 
 int main() {
-  unsigned a, e, i, o, u, blank, tab, newline, ff, fl, fi;
-  a = e = i = o = u = blank = tab = newline = ff = fl = fi = 0;
-  for (string line; getline(cin, line); ++newline) {
-    bool prev = false;
-    for (auto c : line) {
-      if (prev) {
-        if (c == 'f')
-          ++ff;
-        else if (c == 'l')
-          ++fl;
-        else if (c == 'i')
-          ++fi;
-        prev = false;
-      } else if (c == 'f')
-        prev = true;
-      c = tolower(c);
-      if (c == 'a')
-        ++a;
-      else if (c == 'e')
-        ++e;
-      else if (c == 'i')
-        ++i;
-      else if (c == 'o')
-        ++o;
-      else if (c == 'u')
-        ++u;
-      else if (c == ' ')
-        ++blank;
-      else if (c == '\t')
-        ++tab;
+  char c;
+  unsigned aCnt = 0, eCnt = 0, iCnt = 0, oCnt = 0, uCnt = 0, blankCnt = 0,
+           tabCnt = 0, newlineCnt = 0, ffCnt = 0, flCnt = 0, fiCnt = 0;
+  std::cin >> std::noskipws;
+  // Note: A string like "0xffff" counts for 2 ff occurrences.
+  for (bool lastF = false; std::cin >> c; lastF = (c == 'f' && !lastF)) {
+    switch (c) {
+    case 'a':
+    case 'A':
+      ++aCnt;
+      break;
+    case 'e':
+    case 'E':
+      ++eCnt;
+      break;
+    case 'i':
+    case 'I':
+      ++iCnt;
+      break;
+    case 'o':
+    case 'O':
+      ++oCnt;
+      break;
+    case 'u':
+    case 'U':
+      ++uCnt;
+      break;
+    case ' ':
+      ++blankCnt;
+      break;
+    case '\t':
+      ++tabCnt;
+      break;
+    case '\n':
+      ++newlineCnt;
+      break;
+    }
+    if (lastF) {
+      if (c == 'f') {
+        ++ffCnt;
+      } else if (c == 'i') {
+        ++fiCnt;
+      } else if (c == 'l') {
+        ++flCnt;
+      }
     }
   }
-  std::cout << "a\t" << a << std::endl;
-  std::cout << "e\t" << e << std::endl;
-  std::cout << "i\t" << i << std::endl;
-  std::cout << "o\t" << o << std::endl;
-  std::cout << "u\t" << u << std::endl;
-  std::cout << "blank\t" << blank << std::endl;
-  std::cout << "tab\t" << tab << std::endl;
-  std::cout << "newline\t" << newline << std::endl;
-  std::cout << "ff\t" << ff << std::endl;
-  std::cout << "fi\t" << fi << std::endl;
-  std::cout << "fl\t" << fl << std::endl;
+  std::cout << "Number of vowel a: \t" << aCnt << '\n'
+            << "Number of vowel e: \t" << eCnt << '\n'
+            << "Number of vowel i: \t" << iCnt << '\n'
+            << "Number of vowel o: \t" << oCnt << '\n'
+            << "Number of vowel u: \t" << uCnt << '\n'
+            << "Number of blanks: \t" << blankCnt << '\n'
+            << "Number of tabs: \t" << tabCnt << '\n'
+            << "Number of newlines: \t" << newlineCnt << '\n'
+            << "Number of ff counts: \t" << ffCnt << '\n'
+            << "Number of fl counts: \t" << flCnt << '\n'
+            << "Number of fi counts: \t" << fiCnt << std::endl;
   return 0;
 }
