ac1eebeb762edbf430610e8d01329bbb62ad6067
Aaron Fu Lei <aaron.fu@alumni.ust.hk>
2022-01-30 11:58:41 +0800

    Update

--- 08-the-io-library/08-10.cpp
+++ 08-the-io-library/08-10.cpp
@@ -1,3 +1,33 @@
+// 22/01/30 = Sun
+// 18/01/30 = Tue
+// 18/01/16 = Tue
+
 // Exercise 8.10: Write a program to store each line from a file in a
 // vector<string>. Now use an istringstream to read each element from the vector
 // a word at a time.
+
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+
+int main(int argc, char *argv[]) {
+  if (argc != 2) {
+    std::cerr << "Usage: " << argv[0] << " file" << std::endl;
+    return 1;
+  }
+  std::vector<std::string> res;
+  std::ifstream ifs(argv[1]);
+  for (std::string line; std::getline(ifs, line); /* empty */) {
+    res.push_back(line);
+  }
+  for (std::string word; const std::string &line : res) { // C++20
+    std::istringstream iss(line);
+    while (iss >> word) {
+      std::cout << word << ' ';
+    }
+    std::cout << std::endl;
+  }
+  return 0;
+}
--- 08-the-io-library/08-11.cpp
+++ 08-the-io-library/08-11.cpp
@@ -1,5 +1,42 @@
+// 22/01/30 = Sun
+// 18/01/16 = Tue
+
 // Exercise 8.11: The program in this section defined its istringstream object
 // inside the outer while loop. What changes would you need to make if record
 // were defined outside that loop? Rewrite the program, moving the definition of
 // record outside the while, and see whether you thought of all the changes that
 // are needed.
+
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+
+struct PersonInfo {
+  std::string name;
+  std::vector<std::string> phones;
+};
+
+int main() {
+  std::string line, word;
+  std::vector<PersonInfo> people;
+  std::istringstream record;
+  while (std::getline(std::cin, line)) {
+    PersonInfo info;
+    record.str(line);
+    record >> info.name;
+    while (record >> word) {
+      info.phones.push_back(word);
+    }
+    people.push_back(info);
+    record.clear(); // important
+  }
+  for (const PersonInfo &info : people) {
+    std::cout << info.name << ":";
+    for (const std::string &ph : info.phones) {
+      std::cout << " " << ph;
+    }
+    std::cout << std::endl;
+  }
+  return 0;
+}
