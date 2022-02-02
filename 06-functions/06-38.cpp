030d75640eef0c3d31eeaccb305d4ba7f1df0324
Aaron Fu Lei <aaron.fu@alumni.ust.hk>
2022-02-02 22:58:20 +0800

    z

--- 06-functions/06-38.cpp
+++ 06-functions/06-38.cpp
@@ -1,38 +1,42 @@
 // 22/02/02 = Wed
 // 18/02/07 = Wed
 
-// Exercise 6.38: Revise the arrPtr function on to return a reference to the array.
+// Exercise 6.38: Revise the arrPtr function on to return a reference to the
+// array.
 
 // int odd[] = {1,3,5,7,9};
 // int even[] = {0,2,4,6,8};
-// returns a pointer to an array of five int elements
-// decltype(odd) *arrPtr(int i)
-// {
-//     return (i % 2) ? &odd : &even; // returns a pointer to the array
+// decltype(odd) *arrPtr(int i) {
+//   return (i % 2) ? &odd : &even;
 // }
 
 #include <iostream>
 
-using std::cout;
-using std::endl;
+int odd[] = {1, 3, 5, 7, 9};
+int even[] = {0, 2, 4, 6, 8};
+decltype((odd)) arrRef(int i) { return (i % 2) ? odd : even; }
 
-int odd[] = {1,3,5,7,9};
-int even[] = {0,2,4,6,8};
-
-decltype((odd)) arrRef(int i)
-{
-	return (i % 2) ? odd : even;
-}
-
-// access and print out doubled arrays:
+// double elements in the arrays and print them out:
 // 0 4 8 12 16
 // 2 6 10 14 18
-int main()
-{
-	for (auto i : arrRef(0))
-		cout << (i *= 2) << " ";
-	cout << endl;
-	for (auto i : arrRef(1))
-		cout << (i *= 2) << " ";
-	cout << endl;
+void test() {
+  for (int &i : arrRef(0)) {
+    i *= 2;
+  }
+  for (int i : arrRef(0)) {
+    std::cout << i << " ";
+  }
+  std::cout << std::endl;
+  for (int &i : arrRef(1)) {
+    i *= 2;
+  }
+  for (int i : arrRef(1)) {
+    std::cout << i << " ";
+  }
+  std::cout << std::endl;
+}
+
+int main() {
+  test();
+  return 0;
 }
--- 06-functions/README.md
+++ 06-functions/README.md
@@ -602,6 +602,14 @@ I prefer the `using` type declaration introduced in C++11. Modern and readable.
 ### Exercise 6.38
 
 > Revise the `arrPtr` function on to return a reference to the array.
+>
+> ```c++
+> int odd[] = {1,3,5,7,9};
+> int even[] = {0,2,4,6,8};
+> decltype(odd) *arrPtr(int i) {
+>   return (i % 2) ? &odd : &even;
+> }
+> ```
 
 **Answer:**
 
