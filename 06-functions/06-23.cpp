8c498dde1fdfaaee525cb5b66b3a3811a1a0d0eb
Aaron Fu Lei <aaron.fu@alumni.ust.hk>
2022-02-02 21:29:35 +0800

    Update 06-23.cpp

--- 06-functions/06-23.cpp
+++ 06-functions/06-23.cpp
@@ -1,7 +1,9 @@
 // 22/02/02 = Wed
 // 18/02/06 = Tue
 
-// Exercise 6.23: Write your own versions of each of the print functions presented in this section. Call each of these functions to print i and j defined as follows:
+// Exercise 6.23: Write your own versions of each of the print functions
+// presented in this section. Call each of these functions to print i and j
+// defined as follows:
 
 // int i = 0, j[2] = {0, 1};
 
@@ -9,57 +11,38 @@
 #include <iostream>
 #include <iterator>
 
-using std::begin;
-using std::cout;
-using std::end;
-using std::endl;
-using std::size_t;
+void print(const int *p) { std::cout << *p << std::endl; }
 
-void print(const int *);
-void print(const int * b, const int * e);
-void print(const int * a, size_t n);
-void print(int (&a)[2]);
-
-void print(const int * p)
-{
-	cout << *p << endl;
+void print(const int *b, const int *e) {
+  while (b != e) {
+    std::cout << *b++ << std::endl;
+    ;
+  }
 }
 
-void print(const int * b, const int * e)
-{
-	for (const int * p = b; p != e; ++p)
-		cout << *p << " ";
-	cout << endl;
+void print(const int a[], std::size_t n) {
+  for (std::size_t i = 0; i != n; ++i) {
+    std::cout << a[i] << std::endl;
+  }
 }
 
-void print(const int * a, size_t n)
-{
-	for (size_t i = 0; i != n; ++i)
-		cout << a[i] << " ";
-	cout << endl;
+void test() {
+  int i = 0, j[2] = {0, 1};
+  std::cout << "print(&i);" << std::endl;
+  print(&i);
+  std::cout << "print(&i, &i + 1);" << std::endl;
+  print(&i, &i + 1);
+  std::cout << "print(&i, 1);" << std::endl;
+  print(&i, 1);
+  std::cout << "print(j);" << std::endl;
+  print(j);
+  std::cout << "print(std::begin(j), std::end(j));" << std::endl;
+  print(std::begin(j), std::end(j));
+  std::cout << "print(j, 2);" << std::endl;
+  print(j, 2);
 }
 
-void print(int (&a)[2])
-{
-	for (int i = 0; i < 2; ++i)
-		cout << a[i] << " ";
-	cout << endl;
-}
-
-template<size_t N>
-void print(int (&a)[N])
-{
-	for (auto i : a)
-		cout << i << " ";
-	cout << endl;
-}
-
-int main()
-{
-	int i = 0, j[2] = {0, 1};
-	print(&i);
-	print(begin(j), end(j));
-	print(j);
-	print<>(j);	// call template function
-	return 0;
+int main() {
+  test();
+  return 0;
 }
