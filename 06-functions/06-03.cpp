b72604c8233c0d01c7a9d8ef9570c026b5e6df5d
Aaron Fu Lei <aaron.fu@alumni.ust.hk>
2022-02-02 12:39:09 +0800

    Update 06-03.cpp

--- 06-functions/06-03.cpp
+++ 06-functions/06-03.cpp
@@ -1,43 +1,28 @@
-// 21/12/26 = Sun
+// 22/02/02 = Wed
 // 18/02/06 = Tue
 
 // Exercise 6.3: Write and test your own version of fact.
 
-// Exercise 6.4: Write a function that interacts with the user, asking for a number and generating the factorial of that number. Call this function from main.
-
 #include <iostream>
-#include <stdexcept>
 
-using std::cin;
-using std::cout;
-using std::endl;
-using std::invalid_argument;
-
-// Question: Can we define n as an unsigned short?
-// Answer: Yes, because (n * fact(n-1)) converts n into unsigned long long.
-unsigned long long fact(unsigned short n)
-{
-	return n == 0 ? 1ULL : n * fact(n-1);
+int fact(int val) {
+  if (val == 0) {
+    return 1;
+  }
+  int res = 1;
+  for (int i = 1; i <= val; ++i) {
+    res *= i;
+  }
+  return res;
 }
 
-void generate()
-{
-	for (unsigned short n; cout << "Enter an integer n: ", cin >> n; ) {
-		// Note: Curly braces cannot be omitted following try and catch.
-		try {
-			if (n > 20)
-				throw invalid_argument("Invalid argument: n must be in [0, 20].");
-			else
-				cout << n << "! = " << fact(n) << endl;
-		}
-		catch (invalid_argument err) {
-			cout << err.what() << endl;
-		}
-	}
+void test() {
+  for (int i = 0; i <= 10; ++i) {
+    std::cout << "fact(" << i << ") = " << fact(i) << std::endl;
+  }
 }
 
-int main()
-{
-	generate();
-	return 0;
+int main() {
+  test();
+  return 0;
 }
