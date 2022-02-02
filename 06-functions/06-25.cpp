bab59494e7a96d480a68d0536c62585ddd5fb142
Aaron Fu Lei <aaron.fu@alumni.ust.hk>
2022-02-02 21:35:24 +0800

    z

--- 06-functions/06-25.cpp
+++ 06-functions/06-25.cpp
@@ -1,23 +1,17 @@
 // 22/02/02 = Wed
 // 18/02/07 = Wed
 
-// Exercise 6.25: Write a main function that takes two arguments. Concatenate the supplied arguments and print the resulting string.
+// Exercise 6.25: Write a main function that takes two arguments. Concatenate
+// the supplied arguments and print the resulting string.
 
 #include <iostream>
 #include <string>
 
-using std::cerr;
-using std::cout;
-using std::endl;
-using std::string;
-
-int main(int argc, char ** argv)
-{
-	if (argc != 3) {
-		cerr << "Error: Require two arguments!" << endl;
-		return -1;
-	}
-	// convert argv[] to string so that concatenation is easy
-	cout << string(argv[1]) + " " + string(argv[2]) << endl;
-	return 0;
+int main(int argc, char **argv) {
+  if (argc != 3) {
+    std::cerr << "Usage: " << argv[0] << " str1 str2" << std::endl;
+    return 1;
+  }
+  std::cout << std::string(argv[1]) + std::string(argv[2]) << std::endl;
+  return 0;
 }
