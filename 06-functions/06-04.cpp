6fa9e118d59db53be605860200f18fc5f4a4e5db
Aaron Fu Lei <aaron.fu@alumni.ust.hk>
2022-02-02 12:47:41 +0800

    Update 06-04.cpp

--- 06-functions/06-04.cpp
+++ 06-functions/06-04.cpp
@@ -1,3 +1,31 @@
+// 22/02/02 = Wed
+// 18/02/06 = Tue
+
 // Exercise 6.4: Write a function that interacts with the user, asking for a
 // number and generating the factorial of that number. Call this function from
 // main.
+
+#include <iostream>
+
+int fact(int val) {
+  if (val == 0) {
+    return 1;
+  }
+  int res = 1;
+  for (int i = 1; i <= val; ++i) {
+    res *= i;
+  }
+  return res;
+}
+
+void interact() {
+  std::cout << "Please enter a number: ";
+  int i;
+  std::cin >> i;
+  std::cout << "fact(" << i << ") = " << fact(i) << std::endl;
+}
+
+int main() {
+  interact();
+  return 0;
+}
