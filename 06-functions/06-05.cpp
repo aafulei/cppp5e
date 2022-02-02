4e46ea02eb04928706259a694db6ff571243051d
Aaron Fu Lei <aaron.fu@alumni.ust.hk>
2022-02-02 12:52:46 +0800

    Update 06-05.cpp

--- 06-functions/06-05.cpp
+++ 06-functions/06-05.cpp
@@ -1,24 +1,19 @@
-// 21/12/26 = Sun
+// 22/02/02 = Wed
 // 18/02/06 = Tue
 
 // Exercise 6.5: Write a function to return the absolute value of its argument.
 
 #include <iostream>
 
-using std::cin;
-using std::cout;
-using std::endl;
+double abs(double x) { return x >= 0 ? x : -x; }
 
-double abs(double x)
-{
-	return x >= 0 ? x : -x;
+void test() {
+  std::cout << "abs(3.14) = " << abs(3.14) << std::endl;
+  std::cout << "abs(-3.14) = " << abs(-3.14) << std::endl;
+  std::cout << "abs(0) = " << abs(0) << std::endl;
 }
 
-int main()
-{
-	for (double x;
-		cout << "Enter a number: ", cin >> x, cin.ignore(256, '\n');
-		cout << "|" << x << "| = " << abs(x) << endl)
-			;
-	return 0;
+int main() {
+  test();
+  return 0;
 }
