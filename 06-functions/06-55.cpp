e9a82eebca4505a5881fb475c3c669560be9aaa8
Aaron Fu Lei <aaron.fu@alumni.ust.hk>
2022-02-03 16:57:51 +0800

    Update 06-55.cpp

--- 06-functions/06-55.cpp
+++ 06-functions/06-55.cpp
@@ -5,6 +5,10 @@
 // two int values. Store pointers to these functions in your vector from the
 // previous exercise.
 
+// Exercise 6.54: Write a declaration for a function that takes two int
+// parameters and returns an int, and declare a vector whose elements have this
+// function pointer type.
+
 #include <iostream>
 #include <vector>
 
