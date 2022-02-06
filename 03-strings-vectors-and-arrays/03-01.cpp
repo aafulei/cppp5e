25b0aba1c190c73a36e921caf69e2af3a6efbefb
Aaron Fu Lei <aaron.fu@alumni.ust.hk>
2022-02-06 16:30:23 +0800

    Update 03-01.cpp

--- 03-strings-vectors-and-arrays/03-01.cpp
+++ 03-strings-vectors-and-arrays/03-01.cpp
@@ -1,79 +1,44 @@
+// 22/02/06 = Sun
 // 21/12/22 = Wed
 // 18/01/26 = Fri
 
 // Exercise 3.1: Rewrite the exercises from § 1.4.1 (p. 13) and § 2.6.2 (p. 76)
 // with appropriate using declarations.
 
+// Exercise 1.9: Write a program that uses a while to sum the numbers from 50 to
+// 100.
+
+// Exercise 1.10: In addition to the ++ operator that adds 1 to its operand,
+// there is a decrement operator (--) that subtracts 1. Use the decrement
+// operator to write a while that prints the numbers from ten down to zero.
+
+// Exercise 1.11: Write a program that prompts the user for two integers. Print
+// each number in the range specified by those two integers.
+
 // Exercise 2.41: Use your Sales_data class to rewrite the exercises in § 1.5.1
 // (p. 22), § 1.5.2 (p. 24), and § 1.6 (p. 25). For now, you should define your
 // Sales_data class in the same file as your main function.
 
+// Exercise 1.20: http://www.informit.com/title/032174113 contains a copy of
+// Sales_item.h in the Chapter 1 code directory. Copy that file to your working
+// directory. Use it to write a program that reads a set of book sales
+// transactions, writing each transaction to the standard output.
+
+// Exercise 1.21: Write a program that reads two Sales_item objects that have
+// the same ISBN and produces their sum.
+
+// Exercise 1.22: Write a program that reads several transactions for the same
+// ISBN. Write the sum of all the transactions that were read.
+
+// Exercise 1.23: Write a program that reads several transactions and counts how
+// many transactions occur for each ISBN.
+
+// Exercise 1.24: Test the previous program by giving multiple transactions
+// representing multiple ISBNs. The records for each ISBN should be grouped
+// together.
+
 // Exercise 1.25: Using the Sales_item.h header from the Web site, compile and
 // execute the bookstore program presented in this section.
-
-// Note: This program assumes valid input. The records for each ISBN should be
-// grouped together.
-
-/* === Compile and Run ===
-(Linux/macOS)
-clang++ -std=c++11 -pedantic -Wall -Wextra 03-01-bookstore.cpp && \
-./a.out <../data/many-books.txt && \
-./a.out <../data/many-books-no-sales.txt && \
-./a.out <../data/empty.txt
-
-(Windows)
-clang++ -std=c++11 -pedantic -Wall -Wextra 03-01-bookstore.cpp && ^
-a <..\data\many-books.txt && ^
-a <..\data\many-books-no-sales.txt && ^
-a <..\data\empty.txt
-
-=== Input ===
-(many-books.txt)
-0-201-70353-X 4 24.99
-0-201-82470-1 4 45.39
-0-201-88954-4 2 15.00
-0-201-88954-4 5 12.00
-0-201-88954-4 7 12.00
-0-201-88954-4 2 12.00
-0-399-82477-1 2 45.39
-0-399-82477-1 3 45.39
-0-201-78345-X 3 20.00
-0-201-78345-X 2 25.00
-
-(many-books-no-sales.txt)
-0-201-70353-X 0 24.99
-0-201-82470-1 0 45.39
-0-201-88954-4 0 15.00
-0-201-88954-4 0 12.00
-0-201-88954-4 0 12.00
-0-201-88954-4 0 12.00
-0-399-82477-1 0 45.39
-0-399-82477-1 0 45.39
-0-201-78345-X 0 20.00
-0-201-78345-X 0 25.00
-
-(empty.txt)
-
-=== Output ===
-(many-books.txt)
-0-201-70353-X 4 99.96 24.99
-0-201-82470-1 4 181.56 45.39
-0-201-88954-4 16 198 12.375
-0-399-82477-1 5 226.95 45.39
-0-201-78345-X 5 110 22
-
-(two-books-no-sales.txt)
-0-201-70353-X 0 0 (no sales)
-0-201-82470-1 0 0 (no sales)
-0-201-88954-4 0 0 (no sales)
-0-399-82477-1 0 0 (no sales)
-0-201-78345-X 0 0 (no sales)
-
-(empty.txt)
-No data?!
-*/
-
-// Modified from 02-41-bookstore.cpp
 
 #include <iostream>
 #include <string>
