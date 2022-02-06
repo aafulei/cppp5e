592f2a3718e8d496aa6b7b3df221961043076ff7
Aaron Fu Lei <aaron.fu@alumni.ust.hk>
2022-02-06 17:30:50 +0800

    Update 02-41.cpp

--- 02-variables-and-basic-types/02-41.cpp
+++ 02-variables-and-basic-types/02-41.cpp
@@ -1,4 +1,6 @@
+// 22/02/06 = Sun
 // 21/12/21 = Tue
+// 18/02/02 = Fri
 
 // Exercise 2.41: Use your Sales_data class to rewrite the exercises in § 1.5.1
 // (p. 22), § 1.5.2 (p. 24), and § 1.6 (p. 25). For now, you should define your
@@ -30,39 +32,130 @@
 
 struct Sales_data {
   std::string bookNo;
-  unsigned units_sold;
-  double revenue;
+  unsigned units_sold = 0;
+  double revenue = 0.0;
 };
 
-int main() {
-  Sales_data d1, d2;
-  double price;
-  std::cin >> d1.bookNo >> d1.units_sold >> price;
-  if (std::cin) {
-    d1.revenue = d1.units_sold * price;
-  } else {
-    std::cerr << "No data?!" << std::endl;
-    return 1;
+int rewriteExercise_1_20() {
+  Sales_data data;
+  double price = 0;
+  while (std::cin >> data.bookNo >> data.units_sold >> price) {
+    data.revenue = data.units_sold * price;
+    std::cout << data.bookNo << " " << data.units_sold << " " << data.revenue
+              << " " << data.revenue / data.units_sold << std::endl;
   }
-  std::cin >> d2.bookNo >> d2.units_sold >> price;
-  if (std::cin) {
-    d2.revenue = d2.units_sold * price;
-  } else {
-    std::cerr << "No data?!" << std::endl;
-    return 1;
-  }
-  if (d1.bookNo == d2.bookNo) {
-    unsigned total_units = d1.units_sold + d2.units_sold;
-    double total_revenue = d1.revenue + d2.revenue;
-    std::cout << d1.bookNo << " " << total_units << " " << total_revenue << " ";
-    if (total_revenue != 0) {
-      std::cout << (total_revenue / total_units) << std::endl;
+  return 0;
+}
+
+// assume two books have the same ISBN
+int rewriteExercise_1_21() {
+  Sales_data data1, data2;
+  double price = 0;
+  std::cin >> data1.bookNo >> data1.units_sold >> price;
+  data1.revenue = data1.units_sold * price;
+  std::cin >> data2.bookNo >> data2.units_sold >> price;
+  data2.revenue = data2.units_sold * price;
+  if (data1.bookNo == data2.bookNo) {
+    unsigned totalCnt = data1.units_sold + data2.units_sold;
+    double totalRevenue = data1.revenue + data2.revenue;
+    std::cout << data1.bookNo << " " << totalCnt << " " << totalRevenue << " ";
+    if (totalCnt != 0) {
+      std::cout << totalRevenue / totalCnt << std::endl;
     } else {
       std::cout << "(no sales)" << std::endl;
     }
     return 0;
   } else {
     std::cerr << "Data must refer to the same ISBN" << std::endl;
-    return 1;
+    return -1;
   }
+}
+
+// assume multiple books have the same ISBN
+void rewriteExercise_1_22() {
+  Sales_data data, sum;
+  double price = 0;
+  if (std::cin >> data.bookNo >> data.units_sold >> price) {
+    data.revenue = data.units_sold * price;
+    sum = data;
+    while (std::cin >> data.bookNo >> data.units_sold >> price) {
+      data.revenue = data.units_sold * price;
+      sum.units_sold += data.units_sold;
+      sum.revenue += sum.revenue;
+    }
+  }
+  std::cout << sum.bookNo << " " << sum.units_sold << " " << sum.revenue << " "
+            << sum.revenue / sum.units_sold << std::endl;
+}
+
+int rewriteExercise_1_23() {
+  Sales_data currVal, val;
+  double price = 0;
+  if (std::cin >> currVal.bookNo >> currVal.units_sold >> price) {
+    currVal.revenue = currVal.units_sold * price;
+    int cnt = 1;
+    while (std::cin >> val.bookNo >> val.units_sold >> price) {
+      val.revenue = val.units_sold * price;
+      if (val.bookNo == currVal.bookNo) {
+        ++cnt;
+      } else {
+        std::cout << currVal.bookNo << " occurs " << cnt << " times"
+                  << std::endl;
+        currVal = val;
+        cnt = 1;
+      }
+    }
+    std::cout << currVal.bookNo << " occurs " << cnt << " times" << std::endl;
+  }
+  return 0;
+}
+
+int rewriteExercise_1_25() {
+  Sales_data total;
+  double price = 0;
+  if (std::cin >> total.bookNo >> total.units_sold >> price) {
+    total.revenue = total.units_sold * price;
+    Sales_data trans;
+    while (std::cin >> trans.bookNo >> trans.units_sold >> price) {
+      trans.revenue = trans.units_sold * price;
+      if (total.bookNo == trans.bookNo) {
+        total.units_sold += trans.units_sold;
+        total.revenue += trans.revenue;
+      } else {
+        std::cout << total.bookNo << " " << total.units_sold << " "
+                  << total.revenue << " " << total.revenue / total.units_sold
+                  << std::endl;
+        total = trans;
+      }
+    }
+    std::cout << total.bookNo << " " << total.units_sold << " " << total.revenue
+              << " " << total.revenue / total.units_sold << std::endl;
+  } else {
+    std::cerr << "No data?!" << std::endl;
+    return -1;
+  }
+  return 0;
+}
+
+int main() {
+#ifdef RE120
+  return rewriteExercise_1_20();
+#elif RE121
+  return rewriteExercise_1_21();
+#elif RE122
+  return rewriteExercise_1_22();
+#elif RE123
+  return rewriteExercise_1_23();
+#elif RE125
+  return rewriteExercise_1_25();
+#else
+  std::cerr << "Compile options:\n"
+               "\t-D RE120 for rewrite of Exercise 1.20\n"
+               "\t-D RE121 for rewrite of Exercise 1.21\n"
+               "\t-D RE122 for rewrite of Exercise 1.22\n"
+               "\t-D RE123 for rewrite of Exercise 1.23\n"
+               "\t-D RE125 for rewrite of Exercise 1.25"
+            << std::endl;
+#endif
+  return 0;
 }
