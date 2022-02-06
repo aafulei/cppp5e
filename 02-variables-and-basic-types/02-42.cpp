2eaefe8c169208409cfd30b679e6940966dfd8f9
Aaron Fu Lei <aaron.fu@alumni.ust.hk>
2022-02-06 17:37:27 +0800

    2

--- 02-variables-and-basic-types/02-42.cpp
+++ 02-variables-and-basic-types/02-42.cpp
@@ -1,4 +1,6 @@
+// 22/02/06 = Sun
 // 21/12/21 = Tue
+// 18/02/02 = Fri
 
 // Exercise 2.42: Write your own version of the Sales_data.h header and use it
 // to rewrite the exercise from § 2.6.2 (p. 76).
@@ -7,15 +9,46 @@
 // (p. 22), § 1.5.2 (p. 24), and § 1.6 (p. 25). For now, you should define your
 // Sales_data class in the same file as your main function.
 
+// Exercise 1.20: http://www.informit.com/title/032174113 contains a copy of
+// Sales_item.h in the Chapter 1 code directory. Copy that file to your working
+// directory. Use it to write a program that reads a set of book sales
+// transactions, writing each transaction to the standard output.
+
 // Exercise 1.21: Write a program that reads two Sales_item objects that have
 // the same ISBN and produces their sum.
+
+// Exercise 1.22: Write a program that reads several transactions for the same
+// ISBN. Write the sum of all the transactions that were read.
+
+// Exercise 1.23: Write a program that reads several transactions and counts how
+// many transactions occur for each ISBN.
+
+// Exercise 1.24: Test the previous program by giving multiple transactions
+// representing multiple ISBNs. The records for each ISBN should be grouped
+// together.
+
+// Exercise 1.25: Using the Sales_item.h header from the Web site, compile and
+// execute the bookstore program presented in this section.
 
 // Modified from 02-41.cpp
 
 #include "Sales_data.h"
 #include <iostream>
+#include <string>
 
-int main() {
+int rewriteExercise_1_20() {
+  Sales_data data;
+  double price = 0;
+  while (std::cin >> data.bookNo >> data.units_sold >> price) {
+    data.revenue = data.units_sold * price;
+    std::cout << data.bookNo << " " << data.units_sold << " " << data.revenue
+              << " " << data.revenue / data.units_sold << std::endl;
+  }
+  return 0;
+}
+
+// assume two books have the same ISBN
+int rewriteExercise_1_21() {
   Sales_data data1, data2;
   double price = 0;
   std::cin >> data1.bookNo >> data1.units_sold >> price;
@@ -23,12 +56,11 @@ int main() {
   std::cin >> data2.bookNo >> data2.units_sold >> price;
   data2.revenue = data2.units_sold * price;
   if (data1.bookNo == data2.bookNo) {
-    unsigned total_units = data1.units_sold + data2.units_sold;
-    double total_revenue = data1.revenue + data2.revenue;
-    std::cout << data1.bookNo << " " << total_units << " " << total_revenue
-              << " ";
-    if (total_revenue != 0) {
-      std::cout << (total_revenue / total_units) << std::endl;
+    unsigned totalCnt = data1.units_sold + data2.units_sold;
+    double totalRevenue = data1.revenue + data2.revenue;
+    std::cout << data1.bookNo << " " << totalCnt << " " << totalRevenue << " ";
+    if (totalCnt != 0) {
+      std::cout << totalRevenue / totalCnt << std::endl;
     } else {
       std::cout << "(no sales)" << std::endl;
     }
@@ -37,4 +69,93 @@ int main() {
     std::cerr << "Data must refer to the same ISBN" << std::endl;
     return -1;
   }
+}
+
+// assume multiple books have the same ISBN
+void rewriteExercise_1_22() {
+  Sales_data data, sum;
+  double price = 0;
+  if (std::cin >> data.bookNo >> data.units_sold >> price) {
+    data.revenue = data.units_sold * price;
+    sum = data;
+    while (std::cin >> data.bookNo >> data.units_sold >> price) {
+      data.revenue = data.units_sold * price;
+      sum.units_sold += data.units_sold;
+      sum.revenue += sum.revenue;
+    }
+  }
+  std::cout << sum.bookNo << " " << sum.units_sold << " " << sum.revenue << " "
+            << sum.revenue / sum.units_sold << std::endl;
+}
+
+int rewriteExercise_1_23() {
+  Sales_data currVal, val;
+  double price = 0;
+  if (std::cin >> currVal.bookNo >> currVal.units_sold >> price) {
+    currVal.revenue = currVal.units_sold * price;
+    int cnt = 1;
+    while (std::cin >> val.bookNo >> val.units_sold >> price) {
+      val.revenue = val.units_sold * price;
+      if (val.bookNo == currVal.bookNo) {
+        ++cnt;
+      } else {
+        std::cout << currVal.bookNo << " occurs " << cnt << " times"
+                  << std::endl;
+        currVal = val;
+        cnt = 1;
+      }
+    }
+    std::cout << currVal.bookNo << " occurs " << cnt << " times" << std::endl;
+  }
+  return 0;
+}
+
+int rewriteExercise_1_25() {
+  Sales_data total;
+  double price = 0;
+  if (std::cin >> total.bookNo >> total.units_sold >> price) {
+    total.revenue = total.units_sold * price;
+    Sales_data trans;
+    while (std::cin >> trans.bookNo >> trans.units_sold >> price) {
+      trans.revenue = trans.units_sold * price;
+      if (total.bookNo == trans.bookNo) {
+        total.units_sold += trans.units_sold;
+        total.revenue += trans.revenue;
+      } else {
+        std::cout << total.bookNo << " " << total.units_sold << " "
+                  << total.revenue << " " << total.revenue / total.units_sold
+                  << std::endl;
+        total = trans;
+      }
+    }
+    std::cout << total.bookNo << " " << total.units_sold << " " << total.revenue
+              << " " << total.revenue / total.units_sold << std::endl;
+  } else {
+    std::cerr << "No data?!" << std::endl;
+    return -1;
+  }
+  return 0;
+}
+
+int main() {
+#ifdef RE120
+  return rewriteExercise_1_20();
+#elif RE121
+  return rewriteExercise_1_21();
+#elif RE122
+  return rewriteExercise_1_22();
+#elif RE123
+  return rewriteExercise_1_23();
+#elif RE125
+  return rewriteExercise_1_25();
+#else
+  std::cerr << "Compile options:\n\t"
+               "-D RE120 for rewrite of Exercise 1.20\n\t"
+               "-D RE121 for rewrite of Exercise 1.21\n\t"
+               "-D RE122 for rewrite of Exercise 1.22\n\t"
+               "-D RE123 for rewrite of Exercise 1.23\n\t"
+               "-D RE125 for rewrite of Exercise 1.25"
+            << std::endl;
+#endif
+  return 0;
 }
--- 02-variables-and-basic-types/Sales_data.h
+++ 02-variables-and-basic-types/Sales_data.h
@@ -1,9 +1,8 @@
+// 22/02/06 = Sun
 // 21/12/21 = Tue
 
 // Exercise 2.42: Write your own version of the Sales_data.h header and use it
 // to rewrite the exercise from § 2.6.2 (p. 76).
-
-// Exercise 2.40: Write your own version of the Sales_data class.
 
 #ifndef SALES_DATA_H
 #define SALES_DATA_H
@@ -12,8 +11,8 @@
 
 struct Sales_data {
   std::string bookNo;
-  unsigned units_sold;
-  double revenue;
+  unsigned units_sold = 0;
+  double revenue = 0.0;
 };
 
 #endif
