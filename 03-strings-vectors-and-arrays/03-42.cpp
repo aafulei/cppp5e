4d46f78978ab641e9801d8b86c180d78dcbf610f
Aaron Fu Lei <aaron.fu@alumni.ust.hk>
2022-02-10 23:27:30 +0800

    Update 03-42.cpp

--- 03-strings-vectors-and-arrays/03-42.cpp
+++ 03-strings-vectors-and-arrays/03-42.cpp
@@ -1,31 +1,18 @@
+// 22/02/10 = Thu
 // 21/12/23 = Thu
 // 18/01/28 = Sun
 
 // Exercise 3.42: Write a program to copy a vector of ints into an array of
 // ints.
 
-/* === Compile and Run ===
-(Linux/macOS)
-clang++ -std=c++11 -pedantic -Wall -Wextra 03-42-copy-vector-into-array.cpp && \
-./a.out
-
-(Windows)
-clang++ -std=c++11 -pedantic -Wall -Wextra 03-42-copy-vector-into-array.cpp && a
-
-=== Output ===
-1 2 3 4 5
-1 2 3 4 5
-*/
-
-#include <algorithm>
 #include <cstddef>
 #include <iostream>
-#include <iterator>
 #include <vector>
 
 template <typename Seq> void print(Seq const &seq) {
-  for (auto elem : seq)
-    std::cout << elem << " ";
+  for (int i = 0; auto elem : seq) {
+    std::cout << (i++ == 0 ? "" : " ") << elem;
+  }
   std::cout << std::endl;
 }
 
@@ -33,12 +20,10 @@ int main() {
   std::vector<int> v{1, 2, 3, 4, 5};
   constexpr int N = 5;
   int a[N];
-  int b[N];
-  std::copy(v.begin(), v.end(), std::begin(a));
   for (std::size_t i = 0; i != N; ++i) {
-    b[i] = v[i];
+    a[i] = v[i];
   }
+  print(v);
   print(a);
-  print(b);
   return 0;
 }
