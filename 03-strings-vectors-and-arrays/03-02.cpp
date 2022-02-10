937780cb56d6553c5a9939589579fa8ae6250114
Aaron Fu Lei <aaron.fu@alumni.ust.hk>
2022-02-10 15:25:58 +0800

    Update 03-02.cpp

--- 03-strings-vectors-and-arrays/03-02.cpp
+++ 03-strings-vectors-and-arrays/03-02.cpp
@@ -1,16 +1,34 @@
+// 22/02/10 = Thu
 // 21/12/22 = Wed
 // 17/10/20 = Fri
 
 // Exercise 3.2: Write a program to read the standard input a line at a time.
 // Modify your program to read a word at a time.
 
+// Compile with macro READWORD defined to read a word at a time.
+
 #include <iostream>
 #include <string>
 
-int main() {
+void readWord() {
+  std::string word;
+  while (std::cin >> word) {
+    std::cout << word << std::endl;
+  }
+}
+
+void readLine() {
   std::string line;
   while (std::getline(std::cin, line)) {
     std::cout << line << std::endl;
   }
+}
+
+int main() {
+#ifndef READWORD
+  readLine();
+#else
+  readWord();
+#endif
   return 0;
 }
