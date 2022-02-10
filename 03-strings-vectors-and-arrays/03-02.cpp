13fb722b39e1a709aa29ae9d7d6bf11577461879
Aaron Fu Lei <aaron.fu@alumni.ust.hk>
2022-02-10 15:22:06 +0800

    03-02

--- 03-strings-vectors-and-arrays/03-02.cpp
+++ 03-strings-vectors-and-arrays/03-02.cpp
@@ -4,24 +4,6 @@
 // Exercise 3.2: Write a program to read the standard input a line at a time.
 // Modify your program to read a word at a time.
 
-/* === Compile and Run ===
-(Linux/macOS)
-clang++ -std=c++11 -pedantic -Wall -Wextra 03-02-read-lines.cpp && \
-./a.out <../data/fox.txt
-
-(Windows)
-clang++ -std=c++11 -pedantic -Wall -Wextra 03-02-read-lines.cpp && ^
-a <..\data\fox.txt
-
-=== Input ===
-(fox.txt)
-quick fox jumps over the lazy brown dog
-
-=== Output ===
-(fox.txt)
-quick fox jumps over the lazy brown dog
-*/
-
 #include <iostream>
 #include <string>
 
