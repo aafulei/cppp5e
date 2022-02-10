87573a8e7c4b0f495ae25804463f5ab237357475
Aaron Fu Lei <aaron.fu@alumni.ust.hk>
2022-02-10 15:50:55 +0800

    03-04

--- 03-strings-vectors-and-arrays/03-04.cpp
+++ 03-strings-vectors-and-arrays/03-04.cpp
@@ -1,3 +1,4 @@
+// 22/02/10 = Thu
 // 21/12/22 = Wed
 // 17/10/20 = Fri
 
@@ -6,48 +7,42 @@
 // program to report whether the strings have the same length, and if not,
 // report which is longer.
 
-// Note: This program assumes valid input: two strings.
-
-/* === Compile and Run ===
-(Linux/macOS)
-clang++ -std=c++11 -pedantic -Wall -Wextra 03-04-compare-str-sizes.cpp && \
-./a.out <../data/two-strings.txt
-
-(Windows)
-clang++ -std=c++11 -pedantic -Wall -Wextra 03-04-compare-str-sizes.cpp && ^
-a <..\data\two-strings.txt && ^
-a <..\data\two-strings-different-sizes.txt
-
-=== Input ===
-(two-strings.txt)
-clock watch
-
-(two-strings-different-sizes.txt)
-clock timepiece
-
-=== Output ===
-(two-strings.txt)
-Two strings have the same length
-
-(two-strings-different-sizes.txt)
-Second string is longer
-*/
+// Compile with macro STRLEN defined to compare string lengths.
 
 #include <iostream>
 #include <string>
 
+void compareStr(const std::string &s1, const std::string &s2) {
+  if (s1 == s2) {
+    std::cout << "Two strings are equal." << std::endl;
+  } else {
+    if (s1 > s2) {
+      std::cout << "The first string is larger." << std::endl;
+    } else {
+      std::cout << "The second string is larger." << std::endl;
+    }
+  }
+}
+
+void compareStrLen(const std::string &s1, const std::string &s2) {
+  if (s1.size() == s2.size()) {
+    std::cout << "Two strings have the same length." << std::endl;
+  } else {
+    if (s1.size() > s2.size()) {
+      std::cout << "The first string is longer." << std::endl;
+    } else {
+      std::cout << "The second string is longer." << std::endl;
+    }
+  }
+}
+
 int main() {
   std::string s1, s2;
   std::cin >> s1 >> s2;
-  std::string::size_type z1 = s1.size(), z2 = s2.size();
-  if (z1 == z2) {
-    std::cout << "Two strings have the same length" << std::endl;
-  } else {
-    if (z1 > z2) {
-      std::cout << "First string is longer" << std::endl;
-    } else {
-      std::cout << "Second string is longer" << std::endl;
-    }
-  }
+#ifndef STRLEN
+  compareStr(s1, s2);
+#else
+  compareStrLen(s1, s2);
+#endif
   return 0;
 }
